6a57a2a198b65cb145d6d99ac5c6e4c544197686
marciovz <marciovz@yahoo.com.br>
2023-06-12 19:04:14 -0300

    Implementado componente SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
@@ -11,37 +12,39 @@ export function Transaction() {
 
 
     <TransactionsContainer>
-        <TransactionsTable>
-          <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
-              </td>
-              <td>Vendas</td>
-              <td>13/04/2022</td>
-            </tr>
+      <SearchForm />
 
-            <tr>
-              <td width="50%">Hamburger</td>
-              <td>
-                <PriceHighlight variant="outcome"> -R$ 59,00</PriceHighlight>
-              </td>
-              <td>alimentação</td>
-              <td>10/04/2022</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
+      <TransactionsTable>
+        <tbody>
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+            <td>
               <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
-              </td>
-              <td>Vendas</td>
-              <td>13/04/2022</td>
-            </tr>                        
-          </tbody>
-        </TransactionsTable>
-      </TransactionsContainer>
+            </td>
+            <td>Vendas</td>
+            <td>13/04/2022</td>
+          </tr>
+
+          <tr>
+            <td width="50%">Hamburger</td>
+            <td>
+              <PriceHighlight variant="outcome"> -R$ 59,00</PriceHighlight>
+            </td>
+            <td>alimentação</td>
+            <td>10/04/2022</td>
+          </tr>
+
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+            <td>
+            <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+            </td>
+            <td>Vendas</td>
+            <td>13/04/2022</td>
+          </tr>                        
+        </tbody>
+      </TransactionsTable>
+    </TransactionsContainer>
 
   </div>
   )
