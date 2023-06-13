9b2437623d3b2f293288d751fa2b673ebab7c68a
marciovz <marciovz@yahoo.com.br>
2023-06-13 18:53:00 -0300

    Implementado a bibliteca react hook form nos formulários

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,36 @@
 import { MagnifyingGlass } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import { SearchFormContainer } from "./styles";
 
-export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input type="text" placeholder="busque por transações" />
+const searchFormSchema = zod.object({
+  query: zod.string(),
+})
 
-      <button type="submit">
+type SearchFormInputs = zod.infer<typeof searchFormSchema>
+
+export function SearchForm() {
+  const { register, handleSubmit, formState } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema)
+  })
+
+  const { isSubmitting } = formState
+
+  function handleSearchTransactions(data: SearchFormInputs) {
+    console.log(data)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input 
+        type="text" 
+        placeholder="busque por transações" 
+        {...register('query')}  
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
       </button>
     </SearchFormContainer>
